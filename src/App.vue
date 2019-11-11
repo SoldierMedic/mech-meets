ad459443f29a45ea5e2eb0edea9bd352c27a6e57
Marco Rojas <pv2rojas@gmail.com>
2019-11-11 10:14:32 -0700

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list dense>
+        <v-list-item link href="https://discordapp.com/invite/FACZyp9" target="_blank">
+          <v-list-item-action>
+            <v-icon>mdi-discord</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Discord</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color dark>
+      <div class="d-flex align-center">
+        <v-img :src="require('./assets/logo-light.svg')" class="shrink mr-2" contain width="40" />
+        <v-img class="shrink mt-1 hidden-sm-and-down" contain min-width="100" />
+      </div>
+      <v-spacer />
+      <v-toolbar-title>Navigation</v-toolbar-title>
+      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight" />
+    </v-app-bar>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
+  components: {},
+
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
