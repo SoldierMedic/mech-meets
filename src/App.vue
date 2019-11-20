c714a6a85509010af1f9d0ccf76b0d67dc00239c
Marco Rojas <pv2rojas@gmail.com>
2019-11-20 13:11:30 -0700

    added chat

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app right>
-      <v-list dense>
+    <v-navigation-drawer v-model="drawer" app floating right>
+      <v-list dense rounded>
         <v-list-item link href="https://discordapp.com/invite/FACZyp9" target="_blank">
           <v-list-item-action>
             <v-icon>mdi-discord</v-icon>
@@ -9,6 +9,30 @@
 
           <v-list-item-content>
             <v-list-item-title>Discord</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          link
+          href="https://drive.google.com/open?id=1g-0-HS29PFlORcFRmP7pnl22SVB8c3Pf"
+          target="_blank"
+        >
+          <v-list-item-action>
+            <v-icon>mdi-image-multiple</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>Gallery</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link href="https://discordapp.com/invite/FACZyp9" target="_blank">
+          <v-list-item-action>
+            <v-icon>mdi-calendar-question</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>RSVP</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -19,18 +43,21 @@
         <v-img :src="require('./assets/logo-light.svg')" class="shrink mr-2" contain width="40" />
         <v-img class="shrink mt-1 hidden-sm-and-down" contain min-width="100" />
       </div>
-      <v-spacer />
+      <v-spacer></v-spacer>
       <v-toolbar-title>Navigation</v-toolbar-title>
-      <v-app-bar-nav-icon @click.stop="drawerRight = !drawerRight" />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
     </v-app-bar>
+    <Chat />
   </v-app>
 </template>
 
 <script>
-export default {
-  name: "App",
+import Chat from "@/components/chat.vue";
 
-  components: {},
+export default {
+  components: {
+    Chat
+  },
 
   data: () => ({
     drawer: null
---
+++ src/components/chat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="chat-container">
+  <form id="send-container">
+      <input type="text" id="chat-input">
+      <button type="submit" id="send-button">Send</button>
+      </form>     
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.chat-container{
+    ~
+}
+</style>
